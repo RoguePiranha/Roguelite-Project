b090fd205d4137e6e33b39ed23be9db10a3dee7b
Andrew S <akswayze@gmail.com>
2024-09-14 21:30:07 -0600

    Added Godot Game Files

---
+++ Game Files/unnamedroguelike/Assets/Tile Set/RPG.tsx
@@ -0,0 +1,196 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="RPG" tilewidth="32" tileheight="32" tilecount="64" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image source="Ground Tiles/FieldsTile_01.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="Ground Tiles/FieldsTile_02.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="Ground Tiles/FieldsTile_03.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="Ground Tiles/FieldsTile_04.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="Ground Tiles/FieldsTile_05.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="Ground Tiles/FieldsTile_06.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="Ground Tiles/FieldsTile_07.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="Ground Tiles/FieldsTile_08.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="Ground Tiles/FieldsTile_09.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="Ground Tiles/FieldsTile_10.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="Ground Tiles/FieldsTile_11.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="Ground Tiles/FieldsTile_12.png" width="32" height="32"/>
+ </tile>
+ <tile id="13">
+  <image source="Ground Tiles/FieldsTile_13.png" width="32" height="32"/>
+ </tile>
+ <tile id="14">
+  <image source="Ground Tiles/FieldsTile_14.png" width="32" height="32"/>
+ </tile>
+ <tile id="15">
+  <image source="Ground Tiles/FieldsTile_15.png" width="32" height="32"/>
+ </tile>
+ <tile id="16">
+  <image source="Ground Tiles/FieldsTile_16.png" width="32" height="32"/>
+ </tile>
+ <tile id="17">
+  <image source="Ground Tiles/FieldsTile_17.png" width="32" height="32"/>
+ </tile>
+ <tile id="18">
+  <image source="Ground Tiles/FieldsTile_18.png" width="32" height="32"/>
+ </tile>
+ <tile id="19">
+  <image source="Ground Tiles/FieldsTile_19.png" width="32" height="32"/>
+ </tile>
+ <tile id="20">
+  <image source="Ground Tiles/FieldsTile_20.png" width="32" height="32"/>
+ </tile>
+ <tile id="21">
+  <image source="Ground Tiles/FieldsTile_21.png" width="32" height="32"/>
+ </tile>
+ <tile id="22">
+  <image source="Ground Tiles/FieldsTile_22.png" width="32" height="32"/>
+ </tile>
+ <tile id="23">
+  <image source="Ground Tiles/FieldsTile_23.png" width="32" height="32"/>
+ </tile>
+ <tile id="24">
+  <image source="Ground Tiles/FieldsTile_24.png" width="32" height="32"/>
+ </tile>
+ <tile id="25">
+  <image source="Ground Tiles/FieldsTile_25.png" width="32" height="32"/>
+ </tile>
+ <tile id="26">
+  <image source="Ground Tiles/FieldsTile_26.png" width="32" height="32"/>
+ </tile>
+ <tile id="27">
+  <image source="Ground Tiles/FieldsTile_27.png" width="32" height="32"/>
+ </tile>
+ <tile id="28">
+  <image source="Ground Tiles/FieldsTile_28.png" width="32" height="32"/>
+ </tile>
+ <tile id="29">
+  <image source="Ground Tiles/FieldsTile_29.png" width="32" height="32"/>
+ </tile>
+ <tile id="30">
+  <image source="Ground Tiles/FieldsTile_30.png" width="32" height="32"/>
+ </tile>
+ <tile id="31">
+  <image source="Ground Tiles/FieldsTile_31.png" width="32" height="32"/>
+ </tile>
+ <tile id="32">
+  <image source="Ground Tiles/FieldsTile_32.png" width="32" height="32"/>
+ </tile>
+ <tile id="33">
+  <image source="Ground Tiles/FieldsTile_33.png" width="32" height="32"/>
+ </tile>
+ <tile id="34">
+  <image source="Ground Tiles/FieldsTile_34.png" width="32" height="32"/>
+ </tile>
+ <tile id="35">
+  <image source="Ground Tiles/FieldsTile_35.png" width="32" height="32"/>
+ </tile>
+ <tile id="36">
+  <image source="Ground Tiles/FieldsTile_36.png" width="32" height="32"/>
+ </tile>
+ <tile id="37">
+  <image source="Ground Tiles/FieldsTile_37.png" width="32" height="32"/>
+ </tile>
+ <tile id="38">
+  <image source="Ground Tiles/FieldsTile_38.png" width="32" height="32"/>
+ </tile>
+ <tile id="39">
+  <image source="Ground Tiles/FieldsTile_39.png" width="32" height="32"/>
+ </tile>
+ <tile id="40">
+  <image source="Ground Tiles/FieldsTile_40.png" width="32" height="32"/>
+ </tile>
+ <tile id="41">
+  <image source="Ground Tiles/FieldsTile_41.png" width="32" height="32"/>
+ </tile>
+ <tile id="42">
+  <image source="Ground Tiles/FieldsTile_42.png" width="32" height="32"/>
+ </tile>
+ <tile id="43">
+  <image source="Ground Tiles/FieldsTile_43.png" width="32" height="32"/>
+ </tile>
+ <tile id="44">
+  <image source="Ground Tiles/FieldsTile_44.png" width="32" height="32"/>
+ </tile>
+ <tile id="45">
+  <image source="Ground Tiles/FieldsTile_45.png" width="32" height="32"/>
+ </tile>
+ <tile id="46">
+  <image source="Ground Tiles/FieldsTile_46.png" width="32" height="32"/>
+ </tile>
+ <tile id="47">
+  <image source="Ground Tiles/FieldsTile_47.png" width="32" height="32"/>
+ </tile>
+ <tile id="48">
+  <image source="Ground Tiles/FieldsTile_48.png" width="32" height="32"/>
+ </tile>
+ <tile id="49">
+  <image source="Ground Tiles/FieldsTile_49.png" width="32" height="32"/>
+ </tile>
+ <tile id="50">
+  <image source="Ground Tiles/FieldsTile_50.png" width="32" height="32"/>
+ </tile>
+ <tile id="51">
+  <image source="Ground Tiles/FieldsTile_51.png" width="32" height="32"/>
+ </tile>
+ <tile id="52">
+  <image source="Ground Tiles/FieldsTile_52.png" width="32" height="32"/>
+ </tile>
+ <tile id="53">
+  <image source="Ground Tiles/FieldsTile_53.png" width="32" height="32"/>
+ </tile>
+ <tile id="54">
+  <image source="Ground Tiles/FieldsTile_54.png" width="32" height="32"/>
+ </tile>
+ <tile id="55">
+  <image source="Ground Tiles/FieldsTile_55.png" width="32" height="32"/>
+ </tile>
+ <tile id="56">
+  <image source="Ground Tiles/FieldsTile_56.png" width="32" height="32"/>
+ </tile>
+ <tile id="57">
+  <image source="Ground Tiles/FieldsTile_57.png" width="32" height="32"/>
+ </tile>
+ <tile id="58">
+  <image source="Ground Tiles/FieldsTile_58.png" width="32" height="32"/>
+ </tile>
+ <tile id="59">
+  <image source="Ground Tiles/FieldsTile_59.png" width="32" height="32"/>
+ </tile>
+ <tile id="60">
+  <image source="Ground Tiles/FieldsTile_60.png" width="32" height="32"/>
+ </tile>
+ <tile id="61">
+  <image source="Ground Tiles/FieldsTile_61.png" width="32" height="32"/>
+ </tile>
+ <tile id="62">
+  <image source="Ground Tiles/FieldsTile_62.png" width="32" height="32"/>
+ </tile>
+ <tile id="63">
+  <image source="Ground Tiles/FieldsTile_63.png" width="32" height="32"/>
+ </tile>
+ <tile id="64">
+  <image source="Ground Tiles/FieldsTile_64.png" width="32" height="32"/>
+ </tile>
+</tileset>
---
+++ Game Files/unnamedroguelike/Scripts/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+# Declare movement speed
+var speed = 200
+
+# Get input from the player
+func get_input() -> Vector2:
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	return input_vector.normalized()
+
+# Move the player
+func _physics_process(_delta):
+	# Set the built-in velocity property (from CharacterBody2D)
+	velocity = get_input() * speed
+	# Use the move_and_slide method without passing the velocity
+	move_and_slide()
