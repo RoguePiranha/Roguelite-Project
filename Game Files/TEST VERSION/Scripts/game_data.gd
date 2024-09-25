433b9625197fdbe85e8cd5c0515208b222a6caf7
JustinMcMullens <j2mcmullens@gmail.com>
2024-09-25 00:51:59 -0600

    movement changes

--- Game Files/TEST VERSION/Scripts/game_data.gd
+++ Game Files/TEST VERSION/Scripts/game_data.gd
@@ -4,3 +4,14 @@
 #Demon_King:
 	#has_seen[]
 	#has_killed[]
+
+
+var total_playtime: float = 0.0  # In seconds
+var learned_skills: Dictionary = {}  # key: skill_name, value: level or data
+var learned_spells: Dictionary = {}  # key: spell_name, value: level or data
+var completed_quests: Array = []  # List of completed quest_ids
+var classesPlayedAs: Dictionary = {}
+var RacesPlayedAs: Dictionary = {}
+var unlockedStarterClasses: Dictionary = {}
+var unlockedStarterRaces: Dictionary = {}
+var longestRunForEachPlayerClassCombo: Dictionary = {}
